23b3564d717dfc05b9690e02d7a3f5053fb4a8b9
Jason <405339556@qq.com>
2020-08-24 15:14:24 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -23,24 +23,33 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
     {
         $topic = $this->reply->topic;
-        $link =  $topic->link(['#reply' . $this->reply->id]);
+        $link = $topic->link(['#reply' . $this->reply->id]);
 
         // 存入数据库里的数据
         return [
-            'reply_id' => $this->reply->id,
+            'reply_id'      => $this->reply->id,
             'reply_content' => $this->reply->content,
-            'user_id' => $this->reply->user->id,
-            'user_name' => $this->reply->user->name,
-            'user_avatar' => $this->reply->user->avatar,
-            'topic_link' => $link,
-            'topic_id' => $topic->id,
-            'topic_title' => $topic->title,
+            'user_id'       => $this->reply->user->id,
+            'user_name'     => $this->reply->user->name,
+            'user_avatar'   => $this->reply->user->avatar,
+            'topic_link'    => $link,
+            'topic_id'      => $topic->id,
+            'topic_title'   => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+            ->line('你的话题有新回复！')
+            ->action('查看回复', $url);
     }
 }
