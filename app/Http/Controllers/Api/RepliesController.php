b69beecc5943c083bec075842c208e8a04c624ed
Jason <405339556@qq.com>
2020-09-12 11:31:39 +0800

    回复列表

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Queries\ReplyQuery;
 use App\Models\Topic;
 use App\Models\Reply;
 use Illuminate\Http\Request;
@@ -10,6 +11,13 @@ use App\Http\Requests\Api\ReplyRequest;
 
 class RepliesController extends Controller
 {
+    public function index($topicId, ReplyQuery $query)
+    {
+        $replies = $query->where('topic_id', $topicId)->paginate();
+
+        return ReplyResource::collection($replies);
+    }
+
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
         $reply->content = $request->content;
@@ -30,5 +38,12 @@ class RepliesController extends Controller
         $reply->delete();
 
         return response(null, 204);
+    }
+
+    public function userIndex($userId, ReplyQuery $query)
+    {
+        $replies = $query->where('user_id', $userId)->paginate();
+
+        return ReplyResource::collection($replies);
     }
 }
