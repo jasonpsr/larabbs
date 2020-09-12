e4f57d9037070401d278fd5c86ba21ddc01a0781
Jason <405339556@qq.com>
2020-09-12 11:55:37 +0800

    push notification

--- config/app.php
+++ config/app.php
@@ -178,6 +178,9 @@ return [
 
         App\Providers\EasySmsServiceProvider::class,
 
+        App\Providers\EasySmsServiceProvider::class,
+        App\Providers\JpushServiceProvider::class,
+
     ],
 
     /*
